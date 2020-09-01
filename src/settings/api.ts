8a7b7baf01f3164f881f52393b4d212b42c61b52
marcospss <marcospss@gmail.com>
2020-09-01 19:39:03 -0300

    config service

---
+++ src/settings/api.ts
@@ -0,0 +1,7 @@
+export const api = {
+    baseURL: 'https://api.themoviedb.org/3',
+    apikey: '',
+    language: 'en-US',
+  };
+  
+  export const paramsDefault = `api_key=${api.apikey}&language=${api.language}`;

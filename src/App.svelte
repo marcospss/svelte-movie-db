0497b87dbcd62b1dda5e178d35e73776a83f4ca6
marcospss <marcospss@gmail.com>
2020-09-01 21:05:33 -0300

    Load Movies Popular

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,37 @@
 <script lang="ts">
-	import Header from './components/Header.svelte';
-	import Footer from './components/Footer.svelte';
+  import { Movie } from "./services/Movies";
+  import Header from "./components/Header.svelte";
+  import Footer from "./components/Footer.svelte";
+
+  const movies = new Movie();
+  let isLoading = true;
+  let popular;
+  let errorMessage = '';
+  (async () => {
+    try {
+	  const popularResponse = await movies.popular({ page: 1 });
+	  popular = popularResponse.data;
+	  isLoading = false;
+    } catch (error) {
+		errorMessage = error;
+		isLoading = false;
+    }
+  })();
 </script>
+
 <Header />
 <main>
-	<h1>Content</h1>
+	<section>
+		<h1>Popular Movies</h1>
+		{#if isLoading}
+			<p>Loading...</p>
+		{/if}
+		{#if errorMessage}
+			<p>{ errorMessage }</p>
+		{/if}
+		{#if !isLoading && popular}
+			<pre>{JSON.stringify(popular, null, 4)}</pre>
+		{/if}
+	</section>
 </main>
 <Footer />

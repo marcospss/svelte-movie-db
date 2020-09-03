87e19d7a9468719426a82626a120e7c5027b6972
marcospss <marcospss@gmail.com>
2020-09-03 09:36:12 -0300

    Footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,5 +1,42 @@
 <style>
+  footer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+    margin: 2rem 0 0 0;
+    padding: 2rem 0 1rem;
+    background-color: var(--color-blue-black);
+    color: var(--color-white);
+  }
+  footer h2 {
+    margin: 0px;
+    font-size: 1.5rem;
+  }
 
+  footer p {
+    margin: 10px;
+    font-size: 1rem;
+  }
+
+  footer a {
+    font-size: 0.8rem;
+    text-decoration: none;
+    color: var(--color-white);
+  }
+
+  footer a:hover {
+    text-decoration: underline;
+  }
 </style>
 
-<footer>footer</footer>
+<footer>
+  <h2>Let's talk about TMDb</h2>
+  <p>The Movie Database (TMDb) is a community built movie and TV database.</p>
+  <p>
+    <a href="https://www.themoviedb.org/account/signup" target="_blank">
+      <b>Sign up for an account</b>
+    </a>
+  </p>
+</footer>
